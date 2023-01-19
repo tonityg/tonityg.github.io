b5365dac4d22dd0464dacbe8208645c8427fddaa
Toni Gomez <toni.bursty@gmail.com>
2023-01-19 20:47:27 +0100

    deleted scrollbar behaviour + click event buttons + menu projects setup

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ console.log("Initialisé !");
 const projectBtn = document.getElementById("projectbtn")
 const competenceBtn = document.getElementById("competencebtn")
 
+const projectsMenu = document.getElementById("projects")
+
+projectBtn.addEventListener("click", () => {
+    projectBtn.classList.add("activebtn")
+    competenceBtn.classList.remove("activebtn")
+
+    projectsMenu.style.visibility = "visible"
+    projectsMenu.style.bottom = "25px";
+})
+
+competenceBtn.addEventListener("click", () => {
+    competenceBtn.classList.add("activebtn")
+    projectBtn.classList.remove("activebtn")
+})
